81122c95784968c80dcd3b0e79a416f40c471723
Carl Elbaz <carl.elbaz@gmail.com>
2015-01-07 17:33:50 -0500

    Completed Data filtering for seeding, Began rest. controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,24 @@
 class RestaurantsController < ApplicationController
+  # before_filter :require_signed_in!
+  #
+  #   def new
+  #     @place = Restaurant.new
+  #   end
+  #
+  #   def create
+  #     @place = Restaurant.new(place_params)
+  #
+  #     if @place.save
+  #       redirect_to ur
+  #     else
+  #       flash.now[:errors] = @user.errors.full_messages
+  #       render :new
+  #     end
+  #   end
+  #
+  #
+  #   private
+  #   def user_params
+  #     params.require(:user).permit(:password, :username)
+  #   end
 end
