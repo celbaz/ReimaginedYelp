3f1588015380163da5c99ea3a3da794284f16adc
Carl Elbaz <carl.elbaz@gmail.com>
2015-01-07 18:27:06 -0500

    Added Heroku Changes

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,8 @@
 class Restaurant < ActiveRecord::Base
   validates :name, :food_inspection, presence: true
   validates :cuisine, :user_id, :street, :zipcode, :city, :state, presence: true
-  validates :food_inspection,  format: { with: /^[A-F]\z$/, on: :create }
-  # checks the food inspection
+  validates :food_inspection, inclusion: ["A","B","C", "P", "Z"]
+  # checks the food inspection include P and Z
   geocoded_by :address
   after_validation :geocode
 
