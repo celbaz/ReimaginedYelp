905bdcae94206cbc75eb14b8509b13a6af30f37e
Carl Elbaz <carl.elbaz@gmail.com>
2015-01-11 13:56:44 -0500

    heroku bug #1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
 
     if user
       sign_in(user)
-      redirect_to user_url(user)
+      redirect_to root
     else
       flash.now[:errors] = ["Invalid username or password"]
       render :new
