61f2d1a2630bf4ef88ca0538f427b9655e6cb72e
Carl Elbaz <carl.elbaz@gmail.com>
2015-01-11 00:46:56 -0500

    Adding Mapbox Api

--- app/assets/javascripts/views/land_index.js
+++ app/assets/javascripts/views/land_index.js
@@ -1,9 +1,20 @@
 YelpClone.Views.MainPageView = Backbone.View.extend({
   template: JST["landing/index"],
-
+  tagName: "form",
   render: function () {
     var renderedContent = this.template();
     this.$el.html(renderedContent);
     return this;
+  },
+
+  events: {
+    'click .submit-sign': 'LogUser'
+  },
+
+  LogUser: function (event) {
+    event.preventDefault();
+    console.log("hello");
   }
+
+
 });
